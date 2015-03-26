74ffecee7f9323321787db747b1667b4bc975422
aalduz <aldogeneral@gmail.com>
2015-03-26 16:42:34 +0100

    Created the html pages using the views and templates. In the ulrs.py remain all the relations. Also included static folder to apply additional stylesheets if necessary

---
+++ apptask/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.app),
+    url(r'summary/$', views.summary),
+    url(r'summary-average/$', views.summary_average),
+    url(r'^site/(?P<pk>[0-9]+)/$', views.site_detail),    
+)
--- apptask/views.py
+++ apptask/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Record, Site
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Avg
+from decimal import *
 
-# Create your views here.
+getcontext().prec = 2
+
+def app(request):
+    records = Record.objects.order_by('site')
+    sites = Site.objects.all()
+    return render(request, 'app/app.html', {'records': records,'sites': sites})
+
+def site_detail(request, pk):
+    sites = get_object_or_404(Site, pk=pk)
+    records = Record.objects.filter(site=sites.id)
+    return render(request, 'app/site_detail.html', {'records': records,'sites': sites})
+
+def summary(request):    
+    # Aggregation obtained with raw SQL
+    join = Record.objects.raw('SELECT apptask_record.id, apptask_record.created_date, SUM(apptask_record.a_record) AS a_record, SUM(apptask_record.b_record) AS b_record FROM apptask_record INNER JOIN apptask_site ON (apptask_record.site_id = apptask_site.id) GROUP BY apptask_record.site_id ORDER BY apptask_site.id')
+    
+    return render(request, 'app/summary.html', {'summary':join})
+
+def summary_average(request):
+    # Aggregation obtained with Python
+    sites = Site.objects.all()
+    average = []
+    for site in sites:
+    	a = Record.objects.filter(site=site.id).aggregate(Avg('a_record'))
+    	b = Record.objects.filter(site=site.id).aggregate(Avg('b_record'))   	
+    	avg_site = {'site':site.site_name,'a_value':a,'b_value':b}
+    	average.append(avg_site)
+    return render(request, 'app/summary_average.html', {'average': average})
