275601fe0e363be037a6b5262dfd795c1cf943ae
aalduz <aldogeneral@gmail.com>
2015-03-26 17:55:29 +0100

    Updated URL file

--- apptask/urls.py
+++ apptask/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.app),
-    url(r'summary/$', views.summary),
-    url(r'summary-average/$', views.summary_average),
+    url(r'^summary/$', views.summary),
+    url(r'^summary-average/$', views.summary_average),
     url(r'^site/(?P<pk>[0-9]+)/$', views.site_detail),    
 )
